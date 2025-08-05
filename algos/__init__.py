9056c2b05c017dc127fcc1086a676e1758813663
williamwutq <williamwutq@gmail.com>
2025-08-04 18:01:30 -0700

    Add package

---
+++ algos/__init__.py
@@ -0,0 +1,19 @@
+'''
+A package containing various algorithms for the Hex game.
+
+These algorithms can be used for training, benchmarking, and playing the game.
+The algorithms use the following signature:
+```python
+def algorithm(engine: HexEngine, queue: list[Piece]) -> tuple[int, Hex]:
+    ...
+```
+
+All algorithms may raise a `ValueError` if no valid moves are found, or inputs are invalid.
+'''
+
+from .base import random, first
+from .nr import nrsearch, nrminimaxmix, nrminimaxeven, nrentropy, nrsearchworst, nrscoreindex, nrdenseindex, nrbineliminate
+from .rc import rcminimax
+
+__all__ = ['random', 'first', 'nrminimax', 'nrminimaxmix', 'nrminimaxeven', 'nrentropy', 'nrsearch',
+           'nrdenseindex', 'nrscoreindex', 'nrbineliminate', 'nrsearchworst', 'rcminimax']
