e63e7e4edd612ded4657da4485c6ad493a0bf68c
williamwutq <williamwutq@gmail.com>
2025-08-05 14:53:13 -0700

    Create run_0_tf.py

---
+++ run_0_tf.py
@@ -0,0 +1,222 @@
+# Top-level example code for creating a TensorFlow model for HappyHex
+# It is designed to be run directly, not imported as a module.
+# Read the console output carefully and verify the configurations before proceeding.
+# This code assumes you have TensorFlow and Keras installed, and the necessary data files are placed in the correct directories.
+# Adjust the paths and parameters as necessary.
+# Free training and testing data is available at GitHub: `github.com/williamwutq/hpyhexml_data`
+# If you have base algorithm, generate your own data with `hpyhexml.generator`.
+# Copyright (c) 2025 William Wu, licensed under the MIT License.
+
+# Parameters for training the model, replace with your own values.
+save_as = 'hex_tensorflow_mlp_5_1_label_1.keras'
+load_from = None # If you want to load a pre-trained model, specify the path here.
+training_path = ['hpyhexml_data/data/train/nrsearchrank/5-1/0.txt', 
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/1.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/2.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/3.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/4.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/5.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/6.txt',
+                 'hpyhexml_data/data/train/nrsearchrank/5-1/7.txt']
+testing_path = ['hpyhexml_data/data/test/nrsearchrank/5-1.txt']
+initial_lr = 1e-4
+epochs = 100
+batch_size = 64
+clipnorm = 0.8
+
+print("\nStart training script...")
+import time
+print(f"""
+Before proceeding, ensure the following configurations are correct:\n
+- TensorFlow and Keras are installed.
+- hpyhex, hpyhexml, and hpyhexml/tensorflowimpl are placed in the correct directories.
+- Data paths are correct (relative to this running path or absolute paths).
+- Model settings and parameters are reasonable.
+- Softmax uses native labels, not raw scores. Linear correlation data are within reasonable range.
+- Custom loss functions are not in use or do not create sharp gradients.
+- Early stopping is ENABLED unless intentionally disabled.
+- {f"Load model from: {load_from}" if load_from else "Create a new model according to script"}
+- Intended model save path: {save_as}
+- Intended engine radius: 5
+- Intended queue length: 1
+- Intended top choices: <From data>
+- Intended epochs: {epochs}
+- Intended batch size: {batch_size}
+- Intended initial learning rate: {initial_lr}
+""")
+print("If the configurations are correct, script will execute automatically.")
+print("Anytime, Press Ctrl + C to abort.")
+print("Proceed to data loading [y]/n: ", end="")
+response = input().strip().lower()
+if response != 'y' and response:
+    print("Training aborted.")
+    exit(0)
+
+print("\nImporting numpy...")
+import numpy as np
+print("Importing hpyhex and hpyhexml...")
+from hpyhex.hex import HexEngine, Piece, Hex
+from hpyhexml import hex as hx
+from hpyhexml.generator import load_training_data
+
+def prepare_data(engine: HexEngine, queue: list[Piece], desired: list[tuple[int, Hex]]) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Prepare the data for training the MLP model.
+
+    Parameters:
+        engine (HexEngine): The hex engine representing the game state.
+        queue (list[Piece]): The queue of pieces available for placement.
+        desired (list[tuple[int, Hex]]): The desired output for the model, containing tuples of piece index and position.
+    Returns:
+        tuple[np.ndarray, np.ndarray]: A tuple containing the input data and output data as numpy vectors.
+    Raises:
+        TypeError: If any of the parameters are of incorrect type.
+        ValueError: If the Hex position is invalid for the given piece index for any piece in the queue.
+    """
+    input_data = hx.flatten_engine(engine) + hx.flatten_queue(queue)
+    output_data = hx.flatten_single_desired(engine, desired, lambda x: hx.softmax_rank_score(x, len(desired)), swap_noise = 0.01, score_noise = 0)
+    
+    return np.array(input_data), np.array(output_data)
+print("Imported modules.\n")
+
+# Load training data
+print("Loading training data...")
+training_data = []
+for path in training_path:
+    print(f"Loading {path}...")
+    training_data += load_training_data(path)
+np.random.shuffle(training_data)
+print(f"Loaded {len(training_data)} testing samples.")
+print(f"First training sample: \n{training_data[0]}\n")
+
+# Load testing data
+print("Loading testing data...")
+testing_data = []
+for path in testing_path:
+    print(f"Loading {path}...")
+    testing_data += load_training_data(path)
+np.random.shuffle(testing_data)
+print(f"First testing sample: \n{testing_data[0]}\n")
+print(f"Loaded {len(testing_data)} testing samples.")
+print()
+
+# Parse training data
+print("Parsing training data...")
+
+x_train = []
+y_train = []
+for sample in training_data:
+    input_vec, output_vec = prepare_data(*sample)
+    x_train.append(input_vec)
+    y_train.append(output_vec)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+print(f"Parsed {len(x_train)} testing samples.")
+print(f"First training sample: \nInput: \n{x_train[0]}\nOutput: \n{y_train[0]}")
+
+# Parse testing data
+print("Parsing testing data...")
+x_test = []
+y_test = []
+for sample in testing_data:
+    input_vec, output_vec = prepare_data(*sample)
+    x_test.append(input_vec)
+    y_test.append(output_vec)
+
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+print(f"Parsed {len(x_test)} testing samples.")
+print(f"First testing sample: \nInput: \n{x_test[0]}\nOutput: \n{y_test[0]}")
+print()
+
+# Check if data is correct
+print("Data is correct and proceed to training [y]/n: ", end="")
+response = input().strip().lower()
+if response != 'y' and response:
+    print("Training aborted.")
+    exit(0)
+
+print("\nImporting TensorFlow and Keras...")
+import keras
+from keras.metrics import TopKCategoricalAccuracy
+from keras.models import Sequential
+from keras.layers import Dense, Input, Dropout, BatchNormalization, Activation
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
+# Train the model with a larger batch size for better performance
+def create_model():
+    model = Sequential([
+        Input(shape=(68,)),
+        Dense(128, activation=None),
+        BatchNormalization(),
+        Activation('relu'),
+
+        Dense(256, activation=None),
+        BatchNormalization(),
+        Activation('relu'),
+
+        Dense(384, activation=None, kernel_regularizer=l2(2e-7)),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.05),
+
+        Dense(384, activation=None, kernel_regularizer=l2(2e-7)),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.05),
+
+        Dense(256, activation=None),
+        BatchNormalization(),
+        Activation('relu'),
+
+        Dense(122, activation='relu'),
+
+        Dense(61, activation='softmax')
+    ])
+    optimizer = Adam(learning_rate=initial_lr, clipnorm=clipnorm)
+    model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=['accuracy', TopKCategoricalAccuracy(k=5)])
+    return model
+def cosine_decay_schedule(initial_lr, epochs):
+    return keras.optimizers.schedules.CosineDecay(
+        initial_learning_rate=initial_lr,
+        decay_steps=epochs,
+        alpha=0.0
+    )
+cosine_scheduler = keras.callbacks.LearningRateScheduler(
+    lambda epoch: float(cosine_decay_schedule(initial_lr, epochs)(epoch).numpy())
+)
+early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=4, restore_best_weights=True)
+if load_from:
+    print(f"Loading model from {load_from} to be saved as {save_as}...")
+    model = keras.models.load_model(load_from)
+else:
+    print(f"Creating model to be saved as {save_as}...")
+    model = create_model()
+print(f"Start training with {epochs} epochs and initial learning rate of {initial_lr}...")
+model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1, callbacks=[early_stopping])
+print("Training complete.")
+# Save the model for future use
+model.save(save_as)
+print("Start testing on test samples...")
+loss = model.evaluate(x_test, y_test, batch_size=batch_size)
+print("Testing complete.")
+print(f"Test Loss: {loss}\n")
+
+# Benchmark the model
+time.sleep(2)
+print("Benchmarking the model...")
+from hpyhex.benchmark import benchmark, compare
+import algos
+import time
+
+from hpyhexml.tensorflowimpl.autoplayimpl import create_model_predictor
+
+model_predict = create_model_predictor(save_as, "model_predict")
+
+compare(model_predict, algos.nrsearch, 5, 1, eval_times = 1000)
+benchmark(model_predict, 5, 1, eval_times = 400)
+benchmark(algos.random, 5, 1, eval_times = 200)
+benchmark(algos.first, 5, 1, eval_times = 200)
+benchmark(algos.nrsearch, 5, 1, eval_times = 20)
