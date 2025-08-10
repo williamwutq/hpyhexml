2cb034f5fb8b14629a14eec9d14e055f4d6a6f64
williamwutq <williamwutq@gmail.com>
2025-08-09 21:14:22 -0700

    Create rl module

---
+++ hpyhexml/rl/__init__.py
@@ -0,0 +1,6 @@
+'''
+A package for reinforcement learning in the HappyHex game.
+
+This package provides optimized game evironment and training utilities for reinforcement learning algorithms,
+and is faster than the basic simulations provided in the hpyhex.game module.
+'''
